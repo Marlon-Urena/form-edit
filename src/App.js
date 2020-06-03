d41fce6b02e441aeb25e82fccb74ad26ccb4fa32
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-03 22:08:05 +0100

    Completed form-edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import Form from './Form'
 import './App.css';
 
 function App() {
   return (
-    <div>Hello</div>
+    <Form></Form>
   );
 }
 
